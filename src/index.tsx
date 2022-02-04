0ce203cf25744769073ed77f47167458f1da94b8
ramazankarisan <rkarisan25@gmail.com>
2022-02-04 01:14:43 +0100

    create signup comp and add form inside of it

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './store';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const store= createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
  <Provider store={store}>
+   <BrowserRouter>
     <App />
+    </BrowserRouter>
     </Provider>
  ,
   document.getElementById('root')
