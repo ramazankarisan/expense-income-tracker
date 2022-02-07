d1cab0401ace5280aeaee796dac43000ea9aa787
ramazankarisan <rkarisan25@gmail.com>
2022-02-07 23:55:54 +0100

    add composeWithDevTools to redux

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,18 @@ import rootReducer from './store';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 
-const store= createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
- <Provider store={store}>
-   <BrowserRouter>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-    </Provider>
- ,
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
